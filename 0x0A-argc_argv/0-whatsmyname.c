11aa75a6f65fb39e6f84377f4961d029b69c41cb
iKwesi <rbblankson34@gmail.com>
2023-04-10 06:01:37 -0700

    add task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,19 +1,16 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_chessboard - prints the chessboard
- * @a: the array to print
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
-void print_chessboard(char (*a)[8])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	int i, j;
+	printf("%s\n", *argv);
 
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
-		_putchar('\n');
-	}
+	return (0);
 }
