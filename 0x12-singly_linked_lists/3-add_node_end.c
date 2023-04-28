db88f46880d7410d5241a1a8bd30a0bc425fe577
iKwesi <rbblankson34@gmail.com>
2023-04-27 22:57:54 -0700

    update task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -21,28 +21,37 @@ size_t _strlen(const char *s)
 }
 
 /**
- * add_node - adds a new node at the beginning
+ * add_node_end - adds a new node at the beginning
  * of a list_t list.
  * @head: head of the linked list.
  * @str: string to store in the list.
  * Return: address of the head.
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new, *tmp;
 
-	if (!new_node)
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
 	{
+		free(new);
 		return (NULL);
 	}
-	new_node->str = strdup(str);
-	if (!new_node->str)
+	new->len = _strlen(new->str);
+	new->next = NULL;
+	if (*head == NULL)
 	{
-		free(new_node);
-		return (NULL);
+		*head = new;
+		return (new);
 	}
-	new_node->len = _strlen(str);
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
+	tmp = *head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+	return (new);
 }
